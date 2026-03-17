15e20cc11819186bc88d93d55c64c9b5d83c2d3e
guilhermekhub <guilhermefribeirod@gmail.com>
2026-03-17 20:32:07 -0300

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+meme_dict = {
+                "CRINGE": "coisa vergonhosa",
+                "PPRT": "papo reto/é serio"
+                "TMJ": "tamo junto"
+}
+word = imput ("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys()
+
+else:
+    print("nao temos esta palavra no nosso banco de dados")
+
